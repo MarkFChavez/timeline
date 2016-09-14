41e273ee54288ca12d301a034d49ba11e9fb04fb
Mark <markchav3z@gmail.com>
2016-09-14 16:36:51 +0800

    initialize timelines constant

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -1,5 +1,5 @@
 class TimelinesController < ApplicationController
   def index
-    render "timelines/index"
+    render "timelines/index", locals: { timelines: TIMELINES }
   end
 end
---
+++ config/initializers/timelines.rb
@@ -0,0 +1,3 @@
+TIMELINES = [
+  "a", "b", "c"
+]
